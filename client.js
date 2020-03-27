ffe43e2ef3c2e0974d3fb51ba07bd1cbe3c94e63
hlmgnc <61838067+hlmgnc@users.noreply.github.com>
2020-03-27 10:27:01 +0100

    Add files via upload

--- client.js
+++ client.js
@@ -1,10 +1,24 @@
+const Matching = require('./matching')
+const uuid = require('uuid')
+
 class Client{
-    constructor(name, location, demand){
+    constructor(id = uuid.v4(), name, location, demand) {
+    this.id = id
     this.name = name
     this.location = location
     this.demand = demand
     
     } 
+    static create({id, name, location,demand}){
+    const newClient = new Client(name,location)
+    newClients.matchings = matchings
+    
+    return newClient
+    }
+
 }
+
+
+
 
 module.exports = Client 
--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const db = require ('./database')
-const Expert = require ('./expert')
-const Client = require ('./client')
+//const db = require ('./database')
+//const Expert = require ('./expert')
+//const Client = require ('./client')
 const Matching = require ('./matching')
+//const clientDatabase = require ('./client-database')
+//const expertDatabase = require ('./expert-database')
+const {clientDatabase,expertDatabase} = require ('./database')
 
-
-const hilmi = new Expert('Hilmi','cook')
-const armagan = new Client('Armagan','Berlin','cook')
-
+const hilmi = expertDatabase.findByExpert('name', 'Hilmi')
+const armagan = passengerDatabase.findByName('Armagan')
+const aykut = new Client (undefined,'Aykut','Frankfurt', 'driver')
+const dogu = new Expert(undefined,'Dogu','driver')
 hilmi.match(armagan, 'cook')
 
-db.save('clients', [armagan] )
-db.save('experts', [hilmi] )
+//db.save('clients', [{name: 'Armagan ', location: ' Berlin', demand : 'cook'}])
+//db.save('experts', [hilmi] )
+//db.insert('clients', aykut)
+//db.remove('clients',2)
+
+
+const armagan2 = clientDatabase.findByName('client','armagan')
+armagan2.match ('hilmi', 'berlin', 'cook')
+
+clientDatabase.update(armagan)
+
+//Client.forEach(c => console.log (c.name))
 
 const client = db.load('clients')
